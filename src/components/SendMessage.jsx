9756e0d0ed7fcb76ad0ba52b9fe08ea4a7e6ef5f
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-18 15:29:15 +0530

    Update SendMessage.jsx

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useState } from 'react';
 
 const SendMessage = () => {
-  return (
-    <div className='bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg'>
-      <form className='conatinerWrap flex'>
-        <input className='input w-full focus:outline-none bg-gray-100 rounded-r-none' type="text" />
-        <button className='w-auto bg-blue-500 text-white rounded-r-lg px-5 text-sm'>Send</button>
-      </form>
-    </div>
-  )
+    const [value,setValue] = useState("");
+
+    const handleSendMessage = (e) =>{
+        e.preventDefault();
+        console.log(value);
+        setValue("");
+    }
+
+    return (
+        <div className='bg-gray-200 fixed bottom-0 w-full py-10 shadow-lg'>
+            <form onSubmit={handleSendMessage} className='px-2 conatinerWrap flex'>
+                <input placeholder='Enter your message' value={value} onChange={e => setValue(e.target.value)} className='input w-full focus:outline-none bg-gray-100 rounded-r-none' type="text" />
+                <button type='submit' className='w-auto bg-blue-500 text-white rounded-r-lg px-5 text-sm'>Send</button>
+            </form>
+        </div>
+    )
 }
 
 export default SendMessage
