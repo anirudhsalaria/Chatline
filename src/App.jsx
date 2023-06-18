3a8a9aa2e93e0ec8e19fd19d49902d56255af079
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-18 14:30:48 +0530

    daisyUI added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ function App() {
   return (
     <>
       <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <button className="btn btn-primary">Button</button>
       </div>
     </>
   )
