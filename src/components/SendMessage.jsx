eba761bf89f8df0b23b2ffeb17de66040e586dda
Anirudh Salaria <salariaanirudh@gmail.com>
2023-06-20 20:54:56 +0530

    Update SendMessage.jsx

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -5,27 +5,27 @@ import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { db } from '../firebase';
 
 const SendMessage = () => {
-    const [value,setValue] = useState("");
-    const {currentUser} = UserAuth();
+    const [value, setValue] = useState("");
+    const { currentUser } = UserAuth();
 
-    const handleSendMessage = async (e) =>{
+    const handleSendMessage = async (e) => {
         e.preventDefault();
 
-        if(value.trim() === ""){
+        if (value.trim() === "") {
             alert("Enter valid message!");
             return;
         }
 
-        try{
-            const { uid,displayName,photoURL  } = currentUser;
-            await addDoc(collection(db,"messages"),{
+        try {
+            const { uid, displayName, photoURL } = currentUser;
+            await addDoc(collection(db, "messages"), {
                 text: value,
                 name: displayName,
                 avatar: photoURL,
                 createdAt: serverTimestamp(),
                 uid
             })
-        } catch(error){
+        } catch (error) {
             console.log(error);
         }
 
